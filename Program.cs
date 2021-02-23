682668f19270461555d28f44386d3fb416098099
Frebells <leo14bellei@gmail.com>
2021-02-22 21:53:48 -0400

    Criando Repositorio Revisao

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Revisao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string opcaoUsuario = ObterOpcaoUsuario();
+
+            while (opcaoUsuario.ToUpper() != "X")
+            {
+                switch (opcaoUsuario)
+                {
+                    case "1":
+                        //Adicionar aluno
+
+                        break;
+
+                    case "2":
+                        //Listar aluno
+                        break;
+
+                    case "3":
+                        //Calcular media geral
+                        break;
+
+                    default:
+
+                        throw new ArgumentOutOfRangeException();
+
+                        string opcaoUsuario = ObterOpcaoUsuario();
+
+                }
+            }
+
+        }
+
+        private static string ObterOpcaoUsuario()
+        {
+            Console.WriteLine("Informe a opcao desejada:");
+            Console.WriteLine("1 -Inserir novo aluno");
+            Console.WriteLine("2 - Listar alunos");
+            Console.WriteLine("3 - Calcular media geral");
+            Console.WriteLine("X - Sair");
+            Console.WriteLine();
+
+            string opcaoUsuario = Console.ReadLine();
+            return opcaoUsuario;
+        }
+    }
+}
